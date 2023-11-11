ddffd18919f9d599fa6fd6727ba74c1b8087fa0d
unknown <whiy07@gmail.com>
2023-11-11 20:11:45 +0000

    add post request to the url to recieve a request body(2)

--- index.js
+++ index.js
@@ -19,18 +19,37 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const urlDatabase = {};
+let nextShortUrlId = 1;
+
 app.post('/api/shorturl', (req, res) => {
   try {
-    const { original_url, short_url } = req.body
-    if(!original_url) res.json({ message: 'invalid url'})
+    const { original_url } = req.body;
+
+    // Check if the URL is valid
+    const urlPattern = /^(http|https):\/\/www\..+\..+$/;
+    if (!urlPattern.test(original_url)) {
+      res.json({ error: 'invalid url' });
+      return;
+    }
+
+    // Generate a short URL
+    const short_url = nextShortUrlId;
+    nextShortUrlId++;
+
+    // Store the mapping in the database
+    urlDatabase[short_url] = original_url;
+
+    // Respond with the original and short URLs
     res.json({
-    original_url: original_url,
-    short_url: short_url
-  })
+      original_url: original_url,
+      short_url: short_url
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    res.status(500).json({ error: 'internal server error' });
   }
-})
+});
 /*
 * You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. 
   Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
