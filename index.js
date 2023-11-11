ddd998413231c8427b9879b746246de0029cc589
unknown <whiy07@gmail.com>
2023-11-11 20:06:47 +0000

    add post request to the url to recieve a request body

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', (req, res) => {
+  try {
+    const { original_url, short_url } = req.body
+    if(!original_url) res.json({ message: 'invalid url'})
+    res.json({
+    original_url: original_url,
+    short_url: short_url
+  })
+  } catch (error) {
+    console.log(error)
+  }
+})
+/*
+* You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. 
+  Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+* When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+* If you pass an invalid URL that doesn't follow the valid http://www.example.com format, 
+  the JSON response will contain { error: 'invalid url' }
+
+ */
+
+
+
+
+
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
