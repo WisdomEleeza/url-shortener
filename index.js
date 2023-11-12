57aa499f5b923b9ea1ac5864313dc56089be4bdd
unknown <whiy07@gmail.com>
2023-11-12 04:37:10 +0000

    add post request to the url to recieve a request body(13)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+const validUrl = require('valid-url');
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
