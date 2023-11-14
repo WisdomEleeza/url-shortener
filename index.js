f0f698cba55faca43e7ff75b287314004aedbacf
unknown <whiy07@gmail.com>
2023-11-14 08:18:13 +0000

    add updates to packages(7)

--- index.js
+++ index.js
@@ -2,110 +2,67 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const dns = require('dns')
-const urlparser = require('url')
-const { MongoClient  } = require('mongodb');
+const dns = require('dns');
+const urlparser = require('url');
+const { MongoClient } = require('mongodb');
 
-const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const port = process.env.PORT || 3000;
 
-// Connect to MongoDB
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+let db;
+let urls;
+
 async function connectToMongo() {
   try {
+    const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     console.log('Connected to MongoDB');
+    db = client.db('urlshortener');
+    urls = db.collection('urls');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1);
   }
 }
 
-connectToMongo(); // Call the function to connect to MongoDB
+// Call the function to connect to MongoDB before starting the server
+connectToMongo().then(() => {
+  // Your first API endpoint
+  app.post('/api/shorturl', async function (req, res) {
+    const url = req.body.url;
+    const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
+      if (!address) {
+        res.json({ error: 'invalid url' });
+      } else {
+        const urlCount = await urls.countDocuments({});
+        const urlDoc = {
+          url,
+          short_url: urlCount,
+        };
 
-const db = client.db('urlshortener');
-const urls = db.collection('urls');
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.post('/api/shorturl', function(req, res) {
-  const url = req.body.url
-  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async(err, address) => {
-    if(!address) {
-      res.json({ error: 'invalid url'})
-    } else {
-      const urlCount = await urls.countDocuments({})
-      const urlDoc = {
-        url,
-        short_url: urlCount
+        const result = await urls.insertOne(urlDoc);
+        res.json({ original_url: url, short_url: urlCount });
       }
+    });
+  });
 
-      const result = await urls.insertOne(urlDoc)
-      res.json({ original_url: url, short_url: urlCount})
-    }
-  })
-});
+  app.get('/api/shorturl/:short_url', async (req, res) => {
+    const shortUrl = req.params.short_url;
+    const urlDoc = await urls.findOne({ short_url: shortUrl });
+    res.redirect(urlDoc.url);
+  });
 
-app.get('/api/shorturl/:short_url', async (req, res) => {
-  const shortUrl = req.params.short_url
-  const urlDoc = await urls.findOne({ short_url: shortUrl})
-  res.redirect(urlDoc.url)
-})
-
-// // In-memory database to store shortened URLs
-// const urlDatabase = {};
-// let counter = 1;
-
-// // Endpoint to shorten a URL
-// app.post('/api/shorturl', (req, res) => {
-//   // const originalUrl = req.body.original_url;
-//   const { originalUrl } = req.body
-
-//   if (!validUrl.isWebUri(originalUrl)) {
-//     return res.status(400).json({ error: 'invalid url' });
-//   }
-
-//   const shortUrl = counter++;
-//   urlDatabase[shortUrl] = originalUrl;
-
-//   res.json({
-//     original_url: originalUrl,
-//     short_url: shortUrl
-//   });
-// });
-
-// app.get('/api/shorturl/:short_url', (req, res) => {
-//   const shortUrl = parseInt(req.params.short_url);
-
-//   if (urlDatabase.hasOwnProperty(shortUrl)) {
-//     return res.redirect(urlDatabase[shortUrl]);
-//   } else {
-//     return res.status(404).json({ error: 'short url not found' });
-//   }
-// });
-
-
-/**
- * You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties.
-  Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
-  
- * When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
- * If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
- 
- */
-
-
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+  // Start the server
+  app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+  });
 });
