9804f53103bd55f106d841c89dcc7eea56fe450e
unknown <whiy07@gmail.com>
2023-11-14 05:24:10 +0000

    add and update the url-shortener code

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ let counter = 1;
 
 // Endpoint to shorten a URL
 app.post('/api/shorturl', (req, res) => {
-  const originalUrl = req.body.original_url;
+  // const originalUrl = req.body.original_url;
+  const { originalUrl } = req.body
 
   if (!validUrl.isWebUri(originalUrl)) {
     return res.status(400).json({ error: 'invalid url' });
@@ -52,7 +53,14 @@ app.get('/api/shorturl/:short_url', (req, res) => {
 });
 
 
-
+/**
+ * You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties.
+  Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+  
+ * When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+ * If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+ 
+ */
 
 
 
